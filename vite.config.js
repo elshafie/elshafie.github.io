ac534b1c6c9031eb42c712b1c16c763b1f34f3d8
elshafie <m_elshafie15@yahoo.com>
2024-01-08 05:25:11 +0200

    edit portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,13 @@ export default defineConfig({
   assetsInclude: ["**/*.glb"],
   optimizeDeps: {
     exclude: ["js-big-decimal"],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
   },
 });
